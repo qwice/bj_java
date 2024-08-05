2abdedd5486f223ad9224b22407e6c322f6ac019
qwice <134401970+qwice@users.noreply.github.com>
2024-08-05 14:54:40 +0900

    bj_java

---
+++ boj_java/src/bj1001.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class bj1001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        System.out.println(A-B);
+    }
+}
