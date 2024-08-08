13824b2765da33904bec9322e3ecadbfccebd211
qwice <134401970+qwice@users.noreply.github.com>
2024-08-08 17:05:23 +0900

    8/8

---
+++ boj_java/src/Day240808/bj10926.java
@@ -0,0 +1,11 @@
+package Day240808;
+
+import java.util.Scanner;
+
+public class bj10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String Id = sc.next();
+        System.out.println(Id + "??!");
+    }
+}
---
+++ boj_java/src/Day240808/bj11654.java
@@ -0,0 +1,12 @@
+package Day240808;
+
+import java.util.Scanner;
+
+public class bj11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.next();
+        char textCtr = text.charAt(0);
+        System.out.println((int)textCtr);
+    }
+}
